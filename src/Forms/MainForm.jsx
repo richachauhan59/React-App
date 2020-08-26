c1c765f6aae5304ccbc3577057e5d94edcede613
richachauhan59 <richachuahan2019@gmail.com>
2020-08-26 23:19:13 +0530

    Form

---
+++ src/Forms/MainForm.jsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import styles from './styles.module.css'
+import Table from './Table.jsx'
+
+
+export default class Form extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            name :"",
+            age :"",
+            marital : false,
+            role:"Full Stack Developer",
+            gender:"",
+            items:[]
+        }
+    }
+       
+        // full name
+        // age
+        // marital status ( use checkbox )
+        // gender ( use radio button ) ( M / F / Do not prefer to reveal )
+        // role applying for: ( use select tag ) ( Full stack developer / Android developer / Frontend developer )
+
+   
+      HandleSubmit= (e)=>{
+       e.preventDefault();
+       const {name, age, marital, role, gender, items} = this.state
+
+       const data = {
+           name:name,
+           age:age,
+           marital : marital,
+           role:role,
+           gender:gender,
+
+       }
+
+    //    items.push({
+    //      name: this.state.name,
+    //      age: this.state.age,
+    //      marital: this.state.marital,
+    //      role: this.state.role,
+    //      gender:this.state.gender
+    //    });
+   
+       this.setState({
+         items: [...items, data]
+       });
+     };
+   
+     onValueChange = (e) => {
+         const {name, value, checked, type} = e.target;
+        
+         let val = type === "checkbox" ? checked : value
+
+         this.setState({
+             [name]: value
+         })
+     }
+    
+
+    render(){
+        return(
+            <div>
+             <form onSubmit={this.HandleSubmit}>
+
+                 <div>Name: {' '}
+                 <input 
+                 name="name"
+                 type="text"
+                 value={this.state.name}
+                 onChange={this.onValueChange}
+                />
+                 </div>
+                 
+                 <div>
+                 Age: {' '}
+                 <input 
+                 name="age"
+                 value={this.state.age}
+                 onChange={this.onValueChange}
+                 />
+                 </div>
+
+                 <div>
+                 Marital Status: {' '}
+                 <input 
+                 name="marital"
+                 type="checkbox" 
+                 value={this.state.marital}
+                 onChange={this.onValueChange}  
+                 />
+                </div>  
+            
+                <div className="radio">
+                   <div> Select the Gender: {" "}</div>
+                        <label>
+                            <input
+                            name="gender"
+                            type="radio"
+                            value="male"
+                            checked={this.state.gender === "male"}
+                            onChange={this.onValueChange}
+                            />
+                            Male
+                        </label>
+                        </div>
+                        <div className="radio">
+                        <label>
+                            <input
+                            name="gender"
+                            type="radio"
+                            value="female"
+                            checked={this.state.gender === "female"}
+                            onChange={this.onValueChange}
+                            />
+                            Female
+                        </label>
+                        </div>
+
+                 <div>
+                 Role: {' '}
+                 <select 
+                 name="role"
+                 onChange={this.onValueChange}
+                 >
+                     {
+                     ["Full stack developer", "Android developer", "Frontend developer"].map((a)=> (
+                        <option >{a}</option>
+                     ))
+                    }
+                 </select>
+                 </div>
+                 <div>
+                 <input type="submit" value="submit"/>
+                 </div>
+             </form>
+
+             <Table items= {this.state.items }/>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Forms/Table.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './styles.module.css'
+
+export default class Table extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        const items = this.props.items
+        return(
+            <div>
+                <table style={{border:"2px solid black", borderCollapse:"collapse"}}>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Marital Status</th>
+                        <th>Gender</th>
+                        <th>Role Applying for</th>
+                    </tr>
+    
+                    {items.map(item => {
+                        return(
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.age}</td>
+                                <td>{item.marital ? "Married" : "Not Married"}</td>
+                                <td>{item.gender}</td>
+                                <td>{item.role}</td>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </div>
+        )
+    }
+}
